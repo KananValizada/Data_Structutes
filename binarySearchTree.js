82ca264857a24e1ae6f9d7004e249ded281b71e6
KananValizada <kenan.veli@yahoo.com>
2022-03-23 00:19:49 +0400

    binarySearchTree added

---
+++ binarySearchTree.js
@@ -0,0 +1,82 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    //Code here
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          //left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+    //Code here
+    if (!this.root) {
+      return false;
+    } else {
+      let currentNode = this.root;
+      while (currentNode) {
+        if (currentNode.value > value) {
+          currentNode = currentNode.left;
+        } else if (currentNode.value < value) {
+          currentNode = currentNode.right;
+        } else {
+          return currentNode;
+        }
+      }
+      return false;
+    }
+  }
+  // remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.lookup(17));
+let a = JSON.stringify(traverse(tree.root));
+
+console.log(a);
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
